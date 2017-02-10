86a079917acffd35974e94f87025887d7366a816
Daniel Nwankwo <dan_nwankwo@yahoo.co.uk>
2017-02-10 13:25:46 +0000

    form layout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,9 +3,18 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Controller extends Application{
@@ -16,7 +25,6 @@ public class Controller extends Application{
 
         //create event handler
         btn.setOnAction(new EventHandler<ActionEvent>(){
-
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World");
@@ -24,12 +32,41 @@ public class Controller extends Application{
         });
 
         /*stack the pane*/
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
 
         /*set the pane*/
-        Scene scene = new Scene(root,300,150);
-        primaryStage.setTitle("Hello World");
+//        Scene scene = new Scene(root,300,150);
+
+        //set the gridpane
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle,0,0,2,1);
+
+
+        Label userName = new Label("User name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox,1,2);
+        grid.setGridLinesVisible(true);
+
+        /*The Scene*/
+        Scene scene = new Scene(grid, 300, 275);
+
+        primaryStage.setTitle("Welcome JavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
